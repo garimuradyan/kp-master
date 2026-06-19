983654a939184988afed8a65deef2a783f5c86b2
garimuradyan <gari1998vedimail.ru@gmail.com>
2026-06-19 20:04:09 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-window.KP_APP_VERSION='v73-documents-money-like-kp';
+window.KP_APP_VERSION='v76-v74-plus-clear-contract-names';
 var SURL='https://jjfkkvjkjnenwyuiznzv.supabase.co';
 var SKEY='sb_publishable_GH0SGWZlueuHL_jWB4zY5Q_Z8BXdfpI';
 var sb=null;
